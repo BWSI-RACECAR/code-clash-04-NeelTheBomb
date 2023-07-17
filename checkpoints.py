107966f8d7ce81c12708972d4d37e8d626ce290a
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-17 09:11:02 -0400

    Update checkpoints.py

--- checkpoints.py
+++ checkpoints.py
@@ -3,13 +3,15 @@ class Solution:
             # Sort checkpoints from least to greatest
             #type num: list of int
             #return type: int
-            
+            i = 0
             #TODO: Write code below to returnn an int with the solution to the prompt.
-            for i in range(len(checkpoints)-1):
+            while (i <= len(checkpoints))
                 checkpoint = checkpoints[i]
                 if (checkpoint > checkpoint[i + 1]):
                      checkpoint[i] = checkpoint[i+1]
                      checkpoint[i+1] = checkpoint
+                     i = i-1
+                i = i+1
             greatest = 0    
             for i in range(len(checkpoints)-1):
                 if (checkpoints[i+1]-checkpoints[i]) > greatest:
